5ef75788c8cb679ae4cb87902b1df3a9a3f71d9d
STAN Mihai <kazepworld@gmail.com>
2018-10-02 23:40:50 +0200

    Created tests for the command framework

---
+++ CryptoBot.Test/CommandTestUnit.cs
@@ -0,0 +1,78 @@
+﻿using CryptoBot.Commands.Attribute;
+using CryptoBot.Commands.Interface;
+using CryptoBot.Commands.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace CryptoBot.Test
+{
+    [TestClass]
+    public class CommandTestUnit
+    {
+
+        private class GeneralCommandModule : IModule
+        {
+
+            public string DummyString { get; set; }
+            public string FakeDummyString { get; set; }
+
+            [Command("dummy")]
+            public void DummyCommand()
+            {
+                DummyString = "Executed.";
+            }
+
+            [Command("fake-dummy")]
+            public void FakeDummy()
+            {
+                FakeDummyString = "Executed.";
+            }
+
+        }
+
+        private class ArbitraryCommandModule : IModule
+        {
+            public int DummyInteger { get; set; }
+
+            [Command("pimp-my-int")]
+            public void ChangeInteger()
+            {
+                DummyInteger = 420;
+            }
+
+        }
+
+        /// <summary>
+        /// What I expect from this test is to have the first dummy string set correctly to what the command sets it.
+        /// I expect the second(fake) dummy string to be null or something else rather what its own command sets it to.
+        /// If the test is true it means the command handler service executes only the command given and not every method having attribute "CommandAttribute".
+        /// </summary>
+        [TestMethod]
+        public void UseExecuteSingleModule()
+        {
+            var service = new CommandHandlerService();
+            var cmdModule = new GeneralCommandModule();
+            service.RegisterModule(cmdModule);
+            service.ExecuteCommandAsync("dummy").Wait();
+            
+            Assert.AreEqual("Executed.", cmdModule.DummyString);
+            Assert.AreNotEqual("Executed.", cmdModule.FakeDummyString);
+        }
+        
+        [TestMethod]
+        public void UseExecuteMultipleModules()
+        {
+            var service = new CommandHandlerService();
+            var generalCmdModule = new GeneralCommandModule();
+            var arbitraryCmdModule = new ArbitraryCommandModule();
+            service.RegisterModule(generalCmdModule);
+            service.RegisterModule(arbitraryCmdModule);
+            service.ExecuteCommandAsync("dummy").Wait();
+            service.ExecuteCommandAsync("pimp-my-int").Wait();
+
+            Assert.AreEqual("Executed.", generalCmdModule.DummyString);
+            Assert.AreEqual(420, arbitraryCmdModule.DummyInteger);
+        }
+
+    }
+}
---
+++ CryptoBot.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CryptoBot.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CryptoBot.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c2db4231-f7a9-436f-a100-cd0b95d9d716")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
